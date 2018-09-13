a7623db380c82513bd8d9bcffc604e0e2f0cefdd
278097089@qq.com <you@example.com>
2018-09-13 22:52:36 +0800

    add

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <router-link
+          tag="li"
+          class="item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          :to="'/detail/' + item.id"
+        >
+          <img class="item-img" :src="item.imgUrl" />
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+    text-align: left
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 2.7rem
+      height: 1.7rem
+      padding: .2rem
+    .item-info
+      flex: 1
+      padding-top: .1rem
+      min-width: 0
+      text-align left
+      margin-left .1rem
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #333333ba
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
